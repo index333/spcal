579e251925aa392ed6ab66340bf5e3b99e522094
index333 <33111549+index333@users.noreply.github.com>
2018-11-30 16:18:13 +0900

    Add files via upload

---
+++ a2hub.hs
@@ -0,0 +1,31 @@
+import System.IO
+import Graphics.UI.Gtk
+readSample = do
+    r <- getContents
+    return $ words r
+main = do
+    a:b:c:d:e:[] <- readSample
+    print $ a:b:c:d:e:[] 
+    let l = "2.6":b:c:d:e:[]
+    putStrLn $ unwords l
+    initGUI
+    window  <- windowNew
+    hbox <- hBoxNew False 0
+    vbox <- vBoxNew False 0
+    boxPackStart vbox hbox PackNatural 0
+    ls <- mapM (\x -> labelNew (Just x)) l
+    e <- entryNew
+    e `on` entryActivate $ end l e
+    
+    mapM_ (containerAdd hbox) ls
+    containerAdd vbox e
+    containerAdd window vbox
+    widgetShowAll window
+    window `on` unrealize $ end l e
+    mainGUI
+end l e = do
+    hPutStr stderr $ unwords l
+    hPutStr stderr $ " "
+    e' <- e `get` entryText
+    hPutStrLn stderr e'
+    mainQuit
---
+++ a2rim.hs
@@ -0,0 +1,30 @@
+import System.IO
+import MySpinBox
+import Graphics.UI.Gtk
+readSample = do
+    r <- getContents
+    return $ words r
+main = do
+    a:b:c:d:e:[] <- readSample
+    print $ a:b:c:d:e:[] 
+    initGUI
+    window  <- windowNew
+    hbox <- hBoxNew False 0
+    vbox <- vBoxNew False 0
+    boxPackStart vbox hbox PackNatural 0
+    l <- labelNew $ Just a
+    e <- entryNew
+    e `on` entryActivate $ end a e
+    
+    containerAdd vbox l
+    containerAdd vbox e
+    containerAdd window vbox
+    widgetShowAll window
+    window `on` unrealize $ end a e
+    mainGUI
+end a e = do
+    hPutStr stderr a
+    hPutStr stderr $ " "
+    e' <- e `get` entryText
+    hPutStrLn stderr e'
+    mainQuit
