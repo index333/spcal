d7f7dd45ca33109ffe3ecb5fd13063ce25fb2542
index333 <33111549+index333@users.noreply.github.com>
2018-12-11 09:41:32 +0900

    Add files via upload

---
+++ mkHub0.hs
@@ -0,0 +1,45 @@
+import Graphics.UI.Gtk
+import System.IO
+import MySpinBox
+import Round
+import Control.Monad
+main = do
+    initGUI
+    window  <- windowNew
+    hbox <- hBoxNew False 0
+    vbox <- vBoxNew False 0
+    boxPackStart vbox hbox PackNatural 0
+    let names = ["エンド幅(mm)",
+                    "pcd(mm)",
+                    "エンドフランジ距離(mm)",
+                    "エンドフランジ距離(mm)"]
+    adjs <- mkAdjustments [(100,100,135,1,10), 
+                            (40,30, 100, 1,10), 
+                            (30,30, 100, 1,10), 
+                            (30,30,50,1,10)]
+    spins <- myAddSpinButtons hbox names adjs
+    mapM_ (`set` [spinButtonDigits := 0]) spins
+    l <- labelNew $ Just "hub name"
+    e <- entryNew
+    e `on` entryActivate $ end adjs e
+    containerAdd vbox l
+    containerAdd vbox e
+
+    containerAdd window vbox
+    widgetShowAll window
+    window `on` unrealize $ mainQuit
+    mainGUI
+end adjs e = do
+    v0:v1:v2:v3:[] <- mapM (`get` adjustmentValue) adjs
+    let h = v0 / 2
+    hPutStr stderr $ show v1
+    hPutStr stderr " " 
+    hPutStr stderr $ show $ h - v2
+    hPutStr stderr " " 
+    hPutStr stderr $ show v1
+    hPutStr stderr " " 
+    hPutStr stderr $ show $ h - v3
+    hPutStr stderr " " 
+    e' <- e `get` entryText
+    hPutStrLn stderr e'
+    mainQuit
