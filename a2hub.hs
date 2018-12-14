feb454e875e73a83c5f8cc3263578b2a27181c68
index333 <33111549+index333@users.noreply.github.com>
2018-12-14 16:21:14 +0900

    Add files via upload

--- a2hub.hs
+++ a2hub.hs
@@ -1,31 +1,35 @@
 import System.IO
+import Control.Monad
 import Graphics.UI.Gtk
+import ShowButton
 readSample = do
     r <- getContents
-    return $ words r
+    return $ lines r
 main = do
     a:b:c:d:e:[] <- readSample
-    print $ a:b:c:d:e:[] 
-    let l = "2.6":b:c:d:e:[]
-    putStrLn $ unwords l
+    let l = b:c:d:e:[]
+    let ts = ["pcd(mm)",
+                "flange2center(mm)",
+                "pcd(mm)",
+                "flange2center(mm)"]
+    print l
     initGUI
     window  <- windowNew
     hbox <- hBoxNew False 0
     vbox <- vBoxNew False 0
     boxPackStart vbox hbox PackNatural 0
-    ls <- mapM (\x -> labelNew (Just x)) l
+    zipWithM (\x y -> mkShowButton hbox "erd(mm)" (read x)) l ts
     e <- entryNew
-    e `on` entryActivate $ end l e
-    
-    mapM_ (containerAdd hbox) ls
     containerAdd vbox e
+    e `on` entryActivate $ end l e
     containerAdd window vbox
     widgetShowAll window
     window `on` unrealize $ mainQuit
     mainGUI
 end l e = do
-    hPutStr stderr $ unwords l
-    hPutStr stderr $ " "
+    mapM_ (\x -> do hPutStr stderr x
+                    hPutStr stderr $ " "
+                    ) l
     e' <- e `get` entryText
     hPutStrLn stderr e'
     mainQuit
